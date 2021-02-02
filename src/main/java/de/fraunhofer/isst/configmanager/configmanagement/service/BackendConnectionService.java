56339e8ada3cf19b830522f3dc6099cc4b3e929f
Gökhan Kahriman <goekhan.kahriman@isst.fraunhofer.de>
2021-02-02 16:43:14 +0100

    chore: changed logic of creating connector endpoints

--- src/main/java/de/fraunhofer/isst/configmanager/configmanagement/service/BackendConnectionService.java
+++ src/main/java/de/fraunhofer/isst/configmanager/configmanagement/service/BackendConnectionService.java
@@ -11,9 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class BackendConnectionService {
