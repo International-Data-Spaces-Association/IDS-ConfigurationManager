f6ceb5362cda04cb786ab3eb543cbd7a9ab3a987
Tim Berthold <75306992+tmberthold@users.noreply.github.com>
2021-03-23 18:05:47 +0100

    refactor: update versoin number in scheduled log

--- src/main/java/de/fraunhofer/isst/configmanager/ConfigmanagerApplication.java
+++ src/main/java/de/fraunhofer/isst/configmanager/ConfigmanagerApplication.java
@@ -88,6 +88,6 @@ public class ConfigmanagerApplication {
 
     @Scheduled(fixedRate = 60000)
     public void logInfoStillAlive() {
-        log.info("[ConfigManager 5.0.0] Waiting for API call...");
+        log.info("[ConfigManager 6.0.0] Waiting for API call...");
     }
 }
