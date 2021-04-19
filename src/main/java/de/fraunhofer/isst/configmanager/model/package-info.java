954bb0afd2d9f7f90bb22d9c3f12bcd06f47c43c
Tim Berthold <tim.berthold@isst.fraunhofer.de>
2021-04-19 10:10:23 +0200

    docs: add package-info classes

---
+++ src/main/java/de/fraunhofer/isst/configmanager/model/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains the ConfigManager internal data storage classes.
+ *
+ * @version 7.0.0 (last changed at version)
+ * @since 7.0.0 (documented since)
+ * @author IDS-ConfigurationManager Contributors
+ */
+package de.fraunhofer.isst.configmanager.model;
