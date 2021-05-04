a5f09da81e9f5ad8ccc54bacc27748930280cc03
eakker <erik.van.den.akker@isst.fraunhofer.de>
2021-05-04 11:59:45 +0200

    chore: add ContextObjects to Example PetriNet, add new formula

--- src/test/java/de/fraunhofer/isst/configmanager/petrinet/builder/InfomodelPetriNetBuilderTest.java
+++ src/test/java/de/fraunhofer/isst/configmanager/petrinet/builder/InfomodelPetriNetBuilderTest.java
@@ -18,15 +18,26 @@ import de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.*;
 
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeAND.nodeAND;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeEXIST_UNTIL.nodeEXIST_UNTIL;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeExpression.nodeExpression;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeFORALL_MODAL.nodeFORALL_MODAL;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeFORALL_NEXT.nodeFORALL_NEXT;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeFORALL_UNTIL.nodeFORALL_UNTIL;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeMODAL.nodeMODAL;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeNF.nodeNF;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeNOT.nodeNOT;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodeOR.nodeOR;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.state.NodePOS.nodePOS;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.transition.ArcExpression.arcExpression;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.transition.TransitionAF.transitionAF;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.transition.TransitionAND.transitionAND;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.transition.TransitionEXIST_UNTIL.transitionEXIST_UNTIL;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.transition.TransitionFORALL_UNTIL.transitionFORALL_UNTIL;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.transition.TransitionMODAL.transitionMODAL;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.transition.TransitionNOT.transitionNOT;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.FF.FF;
 import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.TT.TT;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.transition.TransitionOR.transitionOR;
+import static de.fraunhofer.isst.configmanager.petrinet.evaluation.formula.transition.TransitionPOS.transitionPOS;
 
 /**
  * Test building a PetriNet from a randomly generated AppRoute
@@ -75,9 +86,9 @@ class InfomodelPetriNetBuilderTest {
         log.info(GraphVizGenerator.generateGraphViz(graph));
         var allPaths = PetriNetSimulator.getAllPaths(graph);
         log.info(allPaths.toString());
-        var formula = nodeAND(nodeMODAL(transitionNOT(FF())), nodeOR(nodeNF(NodeExpression.nodeExpression(x -> true, "testMsg")),TT()));
+        var formula = nodeAND(nodeMODAL(transitionNOT(FF())), nodeOR(nodeNF(nodeExpression(x -> true, "testMsg")),TT()));
         var formula2 = nodeAND(nodeFORALL_NEXT(nodeMODAL(transitionAF(arcExpression(x -> true,"")))), TT());
-        var formula3 = nodeEXIST_UNTIL(nodeMODAL(TT()), nodeNF(NodeExpression.nodeExpression(x -> x.getSourceArcs().isEmpty(), "")));
+        var formula3 = nodeEXIST_UNTIL(nodeMODAL(TT()), nodeNF(nodeExpression(x -> x.getSourceArcs().isEmpty(), "")));
         log.info("Formula 1: " + formula.writeFormula());
         log.info("Result: " + CTLEvaluator.evaluate(formula,graph.getInitial().getNodes().stream().filter(node -> node instanceof Place).findAny().get(), allPaths));
         log.info("Formula 2: " + formula2.writeFormula());
@@ -112,17 +123,29 @@ class InfomodelPetriNetBuilderTest {
         var end = new PlaceImpl(URI.create("place://end"));
         nodes.addAll(List.of(start, copy, init, dat1, dat2, con1, con2, con3, con4, sample, mean, med, rules, stor1, stor2, stor3, stor4, end));
         var initTrans = new TransitionImpl(URI.create("trans://init"));
+        initTrans.setContextObject(new ContextObject(List.of(), null, null, null));
         var getData = new TransitionImpl(URI.create("trans://getData"));
+        getData.setContextObject(new ContextObject(List.of(), null, "data", null));
         var copyData = new TransitionImpl(URI.create("trans://copyData"));
+        copyData.setContextObject(new ContextObject(List.of("france"), "data", "data", null));
         var extract = new TransitionImpl(URI.create("trans://extractSample"));
+        extract.setContextObject(new ContextObject(List.of("france"), "data", "sample", "data"));
         var calcMean = new TransitionImpl(URI.create("trans://calcMean"));
+        calcMean.setContextObject(new ContextObject(List.of("france"), "data", "mean", "data"));
         var calcMed = new TransitionImpl(URI.create("trans://calcMedian"));
+        calcMed.setContextObject(new ContextObject(List.of("france"), "data", "median", "data"));
         var calcRules = new TransitionImpl(URI.create("trans://calcAPrioriRules"));
+        calcRules.setContextObject(new ContextObject(List.of("france", "high_performance"), "data", "rules", "data"));
         var store1 = new TransitionImpl(URI.create("trans://storeData1"));
+        store1.setContextObject(new ContextObject(List.of(), "sample", null, "sample"));
         var store2 = new TransitionImpl(URI.create("trans://storeData2"));
+        store2.setContextObject(new ContextObject(List.of(), "mean", null, "mean"));
         var store3 = new TransitionImpl(URI.create("trans://storeData3"));
+        store3.setContextObject(new ContextObject(List.of(), "median", null, "median"));
         var store4 = new TransitionImpl(URI.create("trans://storeData4"));
+        store4.setContextObject(new ContextObject(List.of(), "rules", null, "rules"));
         var endTrans = new TransitionImpl(URI.create("trans://end"));
+        endTrans.setContextObject(new ContextObject(List.of(), null, null, null));
         nodes.addAll(List.of(initTrans, getData, copyData, extract, calcMean, calcMed, calcRules, store1, store2, store3, store4, endTrans));
         //create arcs
         var arcs = new HashSet<Arc>();
@@ -180,9 +203,37 @@ class InfomodelPetriNetBuilderTest {
         var allPaths = PetriNetSimulator.getAllPaths(graph);
         log.info(String.format("Found %d valid Paths!", allPaths.size()));
         //create formula and evaluate on start node (exists path from start to end)
-        var formula = nodeEXIST_UNTIL(nodeMODAL(TT()), nodeNF(NodeExpression.nodeExpression(x -> x.getSourceArcs().isEmpty(), "")));
+        var formula = nodePOS(nodeNF(nodeExpression(x -> x.getSourceArcs().isEmpty(), "")));
         log.info("Evaluating Formula: " + formula.writeFormula());
-        log.info("Result: " + CTLEvaluator.evaluate(formula,graph.getInitial().getNodes().stream().filter(node -> node.getID().equals(URI.create("place://start"))).findAny().get(), allPaths));
+        log.info("Result: " + CTLEvaluator.evaluate(formula, graph.getInitial().getNodes().stream().filter(node -> node.getID().equals(URI.create("place://start"))).findAny().get(), allPaths));
+        var formulaFrance = nodeMODAL(
+                transitionNOT(
+                        transitionFORALL_UNTIL(
+                                transitionAND(
+                                        transitionAF(arcExpression(transition ->
+                                        {if(transition.getContext().getRead() != null && transition.getContext().getRead().equals("data")) { return transition.getContext().getContext().contains("france"); } return true;}, "")
+                                        ),
+                                        transitionPOS(
+                                                transitionOR(
+                                                        transitionMODAL(nodeNF(nodeExpression(node -> node.getID().equals(URI.create("place://end")), ""))),
+                                                        transitionAF(arcExpression(transition -> transition.getContext().getErase() != null && transition.getContext().getErase().equals("data"), "")))
+                                        )),
+                                transitionAND(
+                                        transitionAF(arcExpression(transition ->
+                                                {if(transition.getContext().getRead() != null && transition.getContext().getRead().equals("data")) {
+                                                    return transition.getContext().getContext().contains("france");
+                                                } return true;}, "")),
+                                        transitionPOS(
+                                                transitionOR(
+                                                        transitionMODAL(nodeNF(nodeExpression(node -> node.getID().equals(URI.create("place://end")), ""))),
+                                                        transitionAF(arcExpression(transition -> transition.getContext().getErase() != null && transition.getContext().getErase().equals("data"), "")))
+                                                )
+                                        )
+                        )
+                )
+        );
+        log.info("Formula France: " + formulaFrance.writeFormula());
+        log.info("Result: " + CTLEvaluator.evaluate(formulaFrance, graph.getInitial().getNodes().stream().filter(node -> node.getID().equals(URI.create("place://start"))).findAny().get(), allPaths));
     }
 
     /**
@@ -204,7 +255,7 @@ class InfomodelPetriNetBuilderTest {
     @Test
     @Disabled
     public void testFormula(){
-        var formula = nodeAND(nodeMODAL(transitionNOT(FF())), nodeOR(nodeNF(NodeExpression.nodeExpression(x -> true, "testMsg")),TT()));
+        var formula = nodeAND(nodeMODAL(transitionNOT(FF())), nodeOR(nodeNF(nodeExpression(x -> true, "testMsg")),TT()));
         log.info(formula.writeFormula());
     }
 }
